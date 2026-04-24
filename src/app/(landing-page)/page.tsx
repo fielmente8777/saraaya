0b15740490b2c2d189493b2048062f4e64558349
Personal <sk7627577@gmail.com>
2026-04-24 20:33:24 +0530

    fix meta data

--- src/app/(landing-page)/page.tsx
+++ src/app/(landing-page)/page.tsx
@@ -1,40 +1,15 @@
 import Form2 from "@/components/forms/Form2";
 import { SectionWithContainer } from "@/components/sectionComponants";
-import { Metadata } from "next";
+import Testimonial from "@/components/sectionComponants/Testimonial";
+import SlidingTitle from "@/components/sliders/SlidingTitle";
+import Banner from "./components/Banner";
+import Experiences from "./components/Experiences";
 import LandingAbout from "./components/LandingAbout";
 import { landingPageData } from "./components/landingPageData";
-import SignatureTherapies from "./components/SignatureTherapies";
-import SlidingTitle from "@/components/sliders/SlidingTitle";
-import Experiences from "./components/Experiences";
 import Rooms from "./components/Rooms";
-import Banner from "./components/Banner";
-import Testimonial from "@/components/sectionComponants/Testimonial";
+import SignatureTherapies from "./components/SignatureTherapies";
 
-export const metadata: Metadata = {
-  title: "Naad Wellness | Luxury Ayurveda & Wellness Retreat Near Delhi NCR",
-  description:
-    "aad Wellness is a luxury wellness retreat near Delhi NCR offering Ayurveda, yoga, meditation and personalised healing programs for mind and body.",
-  alternates: {
-    canonical: "https://naadwellness.com/landing-page",
-  },
-  openGraph: {
-    title: "Naad Wellness | Luxury Ayurveda & Wellness Retreat Near Delhi NCR",
-    description:
-      "aad Wellness is a luxury wellness retreat near Delhi NCR offering Ayurveda, yoga, meditation and personalised healing programs for mind and body.",
-    url: "https://naadwellness.com/landing-page",
-    siteName: "Naad Wellness",
-    locale: "en_IN",
-    type: "website",
-    images: [
-      {
-        url: "https://naadwellness.com/landing-page/og-image.png",
-        width: 800,
-        height: 600,
-        alt: "Naad Wellness",
-      },
-    ],
-  },
-};
+
 
 export default function LandingPage() {
   return (
