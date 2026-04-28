2d2e5e4911fddd946dba8b340670bb73013dbbca
Personal <sk7627577@gmail.com>
2026-04-28 21:08:37 +0530

    up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -65,8 +65,8 @@ export default function RootLayout({
       className={`${playfairDisplay.variable} ${montserrat.variable} h-full antialiased`}
     >
       <head>
-        <script type="text/javascript" id="clarity"  >
-              {`(function(c,l,a,r,i,t,y){
+        <script type="text/javascript" id="clarity">
+          {`(function(c,l,a,r,i,t,y){
             c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
             t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
             y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
