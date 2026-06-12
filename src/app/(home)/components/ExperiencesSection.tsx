2c9abf23f79ca6512ecf61c70bc911d8554224c5
Personal <sk7627577@gmail.com>
2026-06-12 10:18:58 +0530

    fix: update ExperiencesSection layout and improve BannerSlider image loading attributes

--- src/app/(home)/components/ExperiencesSection.tsx
+++ src/app/(home)/components/ExperiencesSection.tsx
@@ -26,7 +26,7 @@ const ExperiencesSection: React.FC<Props> = ({
     <Section defaultPadding={false} className="relative bg-primary">
       <Image src="/home/exp-bg.png" alt="Image" fill className="object-cover" />
       <SectionWithContainer containerClassName="lg:space-y-12 space-y-8">
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8  lg:gap-30">
+        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8  lg:gap-x-30">
           <div className="">
             <p className="flex items-center gap-2 text-background uppercase">
               <span className="w-10 bg-background h-px" />
@@ -37,15 +37,14 @@ const ExperiencesSection: React.FC<Props> = ({
           <div className="">
             <p className="text-white mt-4">{description}</p>
           </div>
-        </div>
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8 lg:gap-30">
+
           {experiences.map((item, index) => (
             <div
-              className="flex flex-col gap-2 mt-6 sticky top-0 z-10 bg-primary"
+              className="flex flex-col gap-2 mt-4  bg-primary"
               key={index}
             >
               <div
-                className={`relative aspect-4/4.5 border-8 border-white w-full ${(index + 1) % 2 === 0 ? "lg:-mt-8" : "lg:mt-10"}`}
+                className={`relative aspect-4/4.25 border-8 border-white w-full ${(index + 1) % 2 === 0 ? "lg:-mt-8" : "lg:mt-6"}`}
               >
                 <Image
                   src={item.image}
@@ -58,7 +57,9 @@ const ExperiencesSection: React.FC<Props> = ({
                 <p className="text-white text-2xl font-primary">{item.title}</p>
                 <p className="text-white text-sm uppercase">{item.price}</p>
               </div>
-              <p className="text-white lg:text-lg max-md:text-sm max-md:text-center">{item.description}</p>
+              <p className="text-white lg:text-lg max-md:text-sm max-md:text-center">
+                {item.description}
+              </p>
             </div>
           ))}
         </div>
--- src/components/sliders/BannerSlider.tsx
+++ src/components/sliders/BannerSlider.tsx
@@ -10,19 +10,26 @@ const BannerSlider = ({ images }: { images: string[] }) => {
         data={images}
         slidesPerView={1}
         spaceBetween={0}
-        modules={[Navigation,Autoplay]}
+        modules={[Navigation, Autoplay]}
         loop
         speed={800}
         autoplay={{ delay: 3500 }}
-        
         navigation={{
           nextEl: ".banner-next",
           prevEl: ".banner-prev",
         }}
         className="w-full"
-        renderSlide={(src) => (
+        renderSlide={(src, index) => (
           <div className="w-full relative md:aspect-[4/1.9] aspect-4/4.5">
-            <Image src={src} alt="Image" fill className="object-cover" />
+            <Image
+              src={src}
+              alt="Image"
+              fill
+              className="object-cover"
+              loading={index === 0 ? "eager" : "lazy"}
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              priority={index === 0 ? true : false}
+            />
           </div>
         )}
       />
