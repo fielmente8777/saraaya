bd3dfe5342a60004f65c3e7d0527af26002e1883
fielmente8777 <122347218+fielmente8777@users.noreply.github.com>
2026-06-12 11:47:07 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -16,17 +16,7 @@ const nextConfig: NextConfig = {
         hostname: "www.facebook.com",
       },
     ],
-    formats: ["image/avif", "image/webp"],
-
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920],
-
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-
-    minimumCacheTTL: 2678400,
-
-    dangerouslyAllowSVG: true,
-
-    contentDispositionType: "attachment",
+    
   },
 
   experimental: {
